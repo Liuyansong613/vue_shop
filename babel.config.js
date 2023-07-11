2efda90f40044fdc1fd1324b1244f3f440fa2465
liuyansong <1602816037@qq.com>
2023-07-11 08:25:17 +0800

    完成登录功能

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk' // 风格样式，可自行更换
+      }
+    ]
   ]
 }
